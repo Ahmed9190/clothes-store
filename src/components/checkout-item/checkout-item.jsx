ccf910c1f1c41de72110a5c4cfefe33bbaff85c2
ahmed9190 <hamadawaled11@gmail.com>
2021-10-03 15:15:35 +0200

    added increase item and decrease item and remove item functionality to checkout item component

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,17 +1,46 @@
 import "./checkout-item.scss";
+import { connect } from "react-redux";
+import {
+  addItem,
+  removeItem,
+  clearItemFromCart,
+} from "./../../redux/cart/cart.actions";
 
-const CheckoutItem = ({ cartItem: { name, price, imageUrl, quantity } }) => {
+const CheckoutItem = ({
+  cartItem,
+  clearItem,
+  decreaseItemQty,
+  increaseItemQty,
+}) => {
+  const { name, price, imageUrl, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => decreaseItemQty(cartItem)}>
+          &#10094;
+        </div>
+
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => increaseItemQty(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  increaseItemQty: (item) => dispatch(addItem(item)),
+  decreaseItemQty: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
